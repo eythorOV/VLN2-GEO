da7da6af20e00677e9701664e518675dc2771e0a
eythorOV <eythorv22@ru.is>
2023-05-03 13:24:37 +0000

    added base for the html

--- geo_pizza/GEO/urls.py
+++ geo_pizza/GEO/urls.py
@@ -19,5 +19,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('playground/', include('playground.urls'))
+    path('playground/', include('playground.urls')),
+    path('home/', include('home.urls')),
+    path('menu/', include('menu.urls')),
 ]
