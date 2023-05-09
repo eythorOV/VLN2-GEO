6fbc298defe51353cc531acd6b23b7332ca02e45
eythorOV <eythorv22@ru.is>
2023-05-09 16:27:36 +0000

    base for cart

--- geo_pizza/menu/urls.py
+++ geo_pizza/menu/urls.py
@@ -3,5 +3,5 @@ from . import views
 
 urlpatterns = [
     path('menu/', views.index, name='menu-index'),
-    path('menu/pizza/', views.pizza_desc, name='pizza-detail'),
+    path('menu/<int:id>', views.pizza_desc, name='pizza-detail'),
 ]
