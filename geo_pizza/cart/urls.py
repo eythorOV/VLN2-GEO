0048e64405f83c80aef105541c7364fc8b6a30ba
eythorOV <eythorv22@ru.is>
2023-05-10 17:54:10 +0000

    new connections in url to checkout pages

--- geo_pizza/cart/urls.py
+++ geo_pizza/cart/urls.py
@@ -3,5 +3,8 @@ from . import views
 
 urlpatterns = [
     path('cart/', views.index, name='cart-page'),
-    path('checkout/contact/', views.checkout, name='checkout-contact-info')
+    path('checkout/contact/', views.contact_info, name='checkout-contact-info'),
+    path('checkout/payment/', views.payment_info, name='checkout-payment-info'),
+    path('checkout/review/', views.review_page, name='checkout-review-info'),
+    path('checkout/complete/', views.complete, name='checkout-complete'),
 ]
--- geo_pizza/cart/views.py
+++ geo_pizza/cart/views.py
@@ -5,5 +5,15 @@ from django.shortcuts import render
 def index(request):
     return render(request, 'cart/cart.html')
 
-def checkout(request):
+def contact_info(request):
     return render(request, 'cart/contact.html', {'title': 'Checkout - Contact Info'})
+
+def payment_info(request):
+    return render(request, 'cart/payment.html', {'title': 'Checkout - Payment Info'})
+
+def review_page(request):
+    return render(request, 'cart/review.html', {'title': 'Checkout - Review'})
+
+def complete(request):
+    return render(request, 'cart/complete.html', {'title': 'Checkout - Complete'})
+
