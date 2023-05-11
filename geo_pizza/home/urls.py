7e42cba841c17fa7d0362cc131ba7c904c94e801
eythorOV <eythorv22@ru.is>
2023-05-11 18:26:21 +0000

    changes in urls and more

--- geo_pizza/home/urls.py
+++ geo_pizza/home/urls.py
@@ -2,7 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home-index'),
+    path('', views.index, name='home'),
     path('home/', views.index, name='home-index'),
     path('home/order/', views.order, name='home-order'),
     path('aboutus/', views.about_us, name='about-us'),
